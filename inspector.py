f76f1f7116740ce837dcacf15fe182339bb54f73
Monica Benitez <mony.23@hotmail.com>
2013-07-03 16:48:54 -0500

    Actualizacion de loginSubmit

--- inspector.py
+++ inspector.py
@@ -14,5 +14,15 @@ def loginform():
               <input type="submit"/> 
               </form>''' 
 
+@post('/login')
+def loginSubmit():
+    name = request.form.get('name')
+    password = request.form.get('password')
+    if checkLogin(name,password):
+        return '<h1> Bienvenido </h1>'
+    else
+        return '<h1> Abrete, estas en mi barrio </h1>'
+
+
 @route('/consulta')
 
