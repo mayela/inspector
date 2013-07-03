63134eaafc9de126c5fb14dfcdf47f3540d4798c
Monica Benitez <mony.23@hotmail.com>
2013-07-03 16:39:36 -0500

    Actualizacion de login

--- inspector.py
+++ inspector.py
@@ -1,9 +1,18 @@
-from bottle import route,run,template
+from bottle import request, route, run, template
 from pymongo import MongoClient
 
 @route('/')
 
-@route('/login')
+@get('/login')
+def loginform():
+    return '''<form method="POST"
+               action="/login">
+              <input type="text"
+                     name="name"/>
+              <input type="password"
+                     name="password"/>
+              <input type="submit"/> 
+              </form>''' 
 
 @route('/consulta')
 
